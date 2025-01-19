e4e99fe18bab3c245ad0a06caf5b759fb3b23b8e
JeanPaulSidi <paulsidi@outlook.com>
2025-01-18 23:54:03 -0800

    Added a class vendingmachine
    Added the vendingmachine class to handle the behavior of the machine

--- VendingMachine1/Program.cs
+++ VendingMachine1/Program.cs
@@ -2,9 +2,58 @@
 {
     internal class Program
     {
+        static void DisplayMenu(string List)
+        {
+            Console.WriteLine("******* VENDING MACHINE *******");
+            Console.WriteLine();
+            Console.WriteLine(List);
+            //Console.WriteLine();
+            Console.WriteLine("*******************************");
+        }
+
+        static int GetUserChoice()
+        {
+            string Userinput;
+            int UserChoice;
+
+            Console.WriteLine();
+            Console.Write("Enter your choice: ");
+            Userinput = Console.ReadLine();
+
+            while (!int.TryParse(Userinput, out UserChoice))
+            {
+                Console.Write("Please enter a whole number: ");
+                Userinput = Console.ReadLine();
+            }
+
+            return UserChoice;
+        }
+
+
         static void Main(string[] args)
         {
-            
+            int UserSelection;
+            string PurchasedItem = string.Empty;
+            string SelectionList = string.Empty;
+            int InventoryCount;
+
+            VendingMachine VendingMachine = new VendingMachine();
+
+            SelectionList = VendingMachine.GetInventoryList();
+            InventoryCount = VendingMachine.GetInventoryCount();
+
+            DisplayMenu(SelectionList);
+            UserSelection = GetUserChoice();
+
+            while (UserSelection <= 0 || UserSelection > InventoryCount)
+            {
+                Console.WriteLine("Input must be between 1 and " + InventoryCount);
+                UserSelection = GetUserChoice();
+            }
+
+            PurchasedItem = VendingMachine.BuyItem(UserSelection);
+            Console.WriteLine();
+            Console.WriteLine("Thank you. Vending of " + PurchasedItem);
         }
     }
 }
---
+++ VendingMachine1/VendingMachine.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VendingMachine1
+{
+    public class VendingMachine
+    {
+        private List<string> _inventory = new List<string>();
+
+        public VendingMachine()
+        {
+            InitializeInventory();
+        }
+
+        public int GetInventoryCount()
+        {
+            return _inventory.Count;
+        }
+
+        public string BuyItem(int Index)
+        {
+            return _inventory[Index - 1];
+        }
+
+        private void AddToInventory(string Item)
+        {
+            _inventory.Add(Item);
+        }
+
+        private void InitializeInventory()
+        {
+            _inventory.Clear();
+
+            AddToInventory("Pepsi");
+            AddToInventory("Coke");
+            AddToInventory("Snack");
+
+        }
+
+        public string GetInventoryList()
+        {
+            string InventoryList = string.Empty;
+
+            for (int i = 0; i < _inventory.Count; i++)
+            {
+                InventoryList += (i + 1) + " " + _inventory[i] + "\n";
+            }
+
+            return InventoryList;
+        }
+
+    }
+}
